82c6726c36f93c793316b27234225cb8b036b848
jacquelynne.heiman <jacquelynne.heiman@gmail.com>
2025-09-09 15:49:35 -0700

    Added a base class for the percentage bar UI so we can create a fuel gauge in the UI.

---
+++ pixel-miner/pixel-miner/Components/UI/PercentageBarUI.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using pixel_miner.Core;
+using SFML.Graphics;
+using SFML.System;
+
+namespace pixel_miner.Components.UI
+{
+    public class PercentageBarUI : Component
+    {
+        // Visual Elements
+        private RectangleShape backgroundBar = null!;
+        private RectangleShape fillBar = null!;
+        private Text labelText = null!;
+
+        // Configuration
+        public Vector2f Position { get; set; } = new Vector2f(20f, 20f);
+        public Vector2f Size { get; set; } = new Vector2f(200f, 20f);
+        public Color BackgroundColor { get; set; } = new Color(255, 255, 255);
+        public Color FillColor { get; set; } = new Color(237, 161, 47);
+        public Color TextColor { get; set; } = Color.White;
+        public bool ShowText { get; set; } = true;
+        public string Label { get; set; } = "";
+        private Font? font;
+
+        // State
+        private float currentPercentage = 1.0f;
+
+        public PercentageBarUI()
+        {
+            InitializeVisualElements();
+        }
+
+        public override void Start()
+        {
+            UpdateVisualElements();
+        }
+
+        public override void Render(RenderWindow window)
+        {
+            window.Draw(backgroundBar);
+            window.Draw(fillBar);
+
+            if (ShowText && font != null)
+            {
+                window.Draw(labelText);
+            }
+        }
+
+        /// <summary>
+        /// Update the percentage (0.0 to 1.0)
+        /// </summary>
+        /// <param name="percentage"></param>
+        public virtual void SetPercentage(float percentage)
+        {
+            currentPercentage = Math.Max(0f, Math.Min(1f, percentage));
+            UpdateFillBar();
+        }
+
+        /// <summary>
+        /// Updates the fill color
+        /// </summary>
+        /// <param name="color"></param>
+        public virtual void SetFillColor(Color color)
+        {
+            FillColor = color;
+            fillBar.FillColor = color;
+        }
+
+        /// <summary>
+        /// Sets the font for text rendering
+        /// </summary>
+        /// <param name="gameFont"></param>
+        public void SetFont(Font gameFont)
+        {
+            font = gameFont;
+            labelText.Font = gameFont;
+        }
+
+        /// <summary>
+        /// Sets the label text
+        /// </summary>
+        /// <param name="label"></param>
+        public void SetLabel(string label)
+        {
+            Label = label;
+            labelText.DisplayedString = label;
+        }
+
+        /// <summary>
+        /// Configure the bar's appearance and position
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="size"></param>
+        /// <param name="backgroundColor"></param>
+        /// <param name="fillColor"></param>
+        /// <param name="showText"></param>
+        public void Configure(Vector2f position, Vector2f size, Color backgroundColor, Color fillColor, Color textColor, bool showText = true)
+        {
+            Position = position;
+            Size = size;
+            BackgroundColor = backgroundColor;
+            FillColor = fillColor;
+            TextColor = textColor;
+            ShowText = showText;
+
+            UpdateVisualElements();
+        }
+
+        private void InitializeVisualElements()
+        {
+            backgroundBar = new RectangleShape();
+            fillBar = new RectangleShape();
+
+            labelText = new Text();
+            labelText.CharacterSize = 14;
+            labelText.FillColor = Color.White;
+        }
+
+        private void UpdateVisualElements()
+        {
+            backgroundBar.Size = Size;
+            backgroundBar.Position = Position;
+            backgroundBar.FillColor = BackgroundColor;
+
+            UpdateFillBar();
+
+            labelText.Position = new Vector2f(Position.X, Position.Y + Size.Y + 5f);
+            labelText.DisplayedString = Label;
+        }
+
+        private void UpdateFillBar()
+        {
+            float currentWidth = Size.X * currentPercentage;
+            fillBar.Size = new Vector2f(currentWidth, Size.Y);
+            fillBar.Position = Position;
+            fillBar.FillColor = FillColor;
+        }
+    }
+}
--- pixel-miner/pixel-miner/Scenes/Factories/GameSceneFactory.cs
+++ pixel-miner/pixel-miner/Scenes/Factories/GameSceneFactory.cs
@@ -2,8 +2,10 @@ using pixel_miner.Components.Gameplay;
 using pixel_miner.Components.Input;
 using pixel_miner.Components.Movement;
 using pixel_miner.Components.Rendering;
+using pixel_miner.Components.UI;
 using pixel_miner.Core;
 using SFML.System;
+using SFML.Graphics;
 
 namespace pixel_miner.Scenes.Factories
 {
@@ -27,14 +29,31 @@ namespace pixel_miner.Scenes.Factories
 
             // Create camera that follows player
             var camera = CreateBasicCamera(player);
+            var cameraComponent = camera.GetComponent<Camera>();
             scene.AddGameObject(camera);
 
             // Add some test reference objects
-            var ref1 = CreateTestObject("Ref1", 200, 200, SFML.Graphics.Color.Green);
-            var ref2 = CreateTestObject("Ref2", -200, -200, SFML.Graphics.Color.Blue);
+            var ref1 = CreateTestObject("Ref1", 200, 200, Color.Green);
+            var ref2 = CreateTestObject("Ref2", -200, -200, Color.Blue);
 
             scene.AddGameObject(ref1);
             scene.AddGameObject(ref2);
+
+            // Add a percentage bar to the UI
+            var percentageBar = new GameObject("TestPercentageBar");
+            var percentageBarUI = percentageBar.AddComponent<PercentageBarUI>();
+
+            percentageBarUI.Configure(
+                position: cameraComponent != null ? cameraComponent.ScreenToWorld(new Vector2f(20f, 20f)) : new Vector2f(20f, 20f),
+                size: new Vector2f(200f, 20f),
+                backgroundColor: new Color(255, 255, 255),
+                fillColor: new Color(237, 161, 47),
+                textColor: Color.White
+            );
+
+            percentageBarUI.SetPercentage(0.75f);
+
+            scene.AddGameObject(percentageBar);
 
             return scene;
         }
